2de633bda3263abf104c009005fdeb64dbf29410
Deepali2 <hdgarg@gmail.com>
2019-06-19 14:56:05 -0700

    adds user middleware

--- models/user.js
+++ models/user.js
@@ -21,6 +21,29 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+//hook to hash the password
+userSchema.pre('save', async function(next) {
+  try {
+    if(!this.isModified('password')) {
+      return next(); // if you have not changed the password then don't go in and hash it again
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);  //10 is for the salting of the password; await because bcrypt.hash isn't an async action
+    this.password = hashedPassword;
+    return next(); //will move to saving the password in the database
+  } catch(err) {
+    return next(err); //this will go to our error handler
+  }
+});
+//password comparison
+userSchema.method.comparePassword = async function(candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch(err) {
+    return next(err);
+  }
+}
+
 const User = mongoose.Model('User', userSchema);
 
 module.exports = User;
