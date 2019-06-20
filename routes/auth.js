bb21eb576f5159fbe4535d80fbf52b5d7d77e8e8
Deepali2 <hdgarg@gmail.com>
2019-06-19 21:51:40 -0700

    adds signin functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const express     = require('express'),
-       router     = express.Router(),
-       { signup } = require("../handlers/auth");
+const express             = require('express'),
+       router             = express.Router(),
+       { signup, signin } = require("../handlers/auth");
 
 //if there is any kind of post request to /signup then I want to run my signup function
 router.post("/signup", signup);
-
+router.post("/signin", signin);
 
 module.exports = router;
